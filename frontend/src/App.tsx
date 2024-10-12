d13794362e132f434bcb409cc800fe9b59e4ff56
Austin Kong <konghoitec@gmail.com>
2024-10-12 19:17:23 +0800

    Extract navbar into separate component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,15 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Stack, CircularProgress } from "@mui/material";
+import session from "./api/sessions_manager";
+
 import Homepage from "./pages/Homepage";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Workshops from "./pages/workshops/Workshops";
 import WorkshopDetails from "./pages/workshops/WorkshopDetails";
-import {
-  Routes,
-  Route,
-  BrowserRouter,
-  Link as RouterLink,
-} from "react-router-dom";
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-  Stack,
-  CircularProgress,
-} from "@mui/material";
-import session from "./api/sessions_manager";
+import Navbar from "./components/Navbar";
 
 /* Check if JWT_Token is stored and attempt to log the user in with that token */
 async function authenticate(): Promise<boolean> {
@@ -70,74 +60,7 @@ function App() {
     <session.SessionContext.Provider value={{ isAuth, setIsAuth }}>
       <div className="App">
         <BrowserRouter>
-          <AppBar position="static" elevation={0}>
-            <Toolbar
-              style={{ display: "flex", justifyContent: "space-between" }}
-            >
-              <Stack spacing={2} direction="row" alignItems="center">
-                <Typography
-                  sx={{ textDecoration: "none", fontWeight: "bold" }}
-                  variant="h5"
-                  color="inherit"
-                  component={RouterLink}
-                  to="/"
-                >
-                  PSA-Codesprint-2024
-                </Typography>
-                {isAuth && (
-                  <Typography variant="h6">
-                    Welcome {session.getters.getUser().username}!
-                  </Typography>
-                )}
-              </Stack>
-              <div>
-                {!isAuth ? (
-                  <Stack spacing={2} direction="row">
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      component={RouterLink}
-                      to="/login"
-                    >
-                      Login
-                    </Button>
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      component={RouterLink}
-                      to="/register"
-                    >
-                      Register
-                    </Button>
-                  </Stack>
-                ) : (
-                  <Stack spacing={2} direction="row">
-                    <Button
-                      variant="contained"
-                      component={RouterLink}
-                      to="/post_question"
-                    >
-                      Post Question
-                    </Button>
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      onClick={() => {
-                        session.actions
-                          .logoutUser()
-                          .then(() => setIsAuth(false))
-                          .catch(() => {
-                            alert("Error logging out, refresh the page!");
-                          });
-                      }}
-                    >
-                      Logout
-                    </Button>
-                  </Stack>
-                )}
-              </div>
-            </Toolbar>
-          </AppBar>
+          <Navbar />
 
           <Routes>
             <Route path="/" element={<Homepage />} />
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,79 @@
+import { Link as RouterLink } from "react-router-dom";
+import { useContext } from "react";
+import { AppBar, Toolbar, Typography, Button, Stack } from "@mui/material";
+import session from "../api/sessions_manager";
+
+const Navbar = () => {
+  const { isAuth, setIsAuth } = useContext(session.SessionContext);
+
+  return (
+    <AppBar position="static" elevation={0}>
+      <Toolbar style={{ display: "flex", justifyContent: "space-between" }}>
+        <Stack spacing={2} direction="row" alignItems="center">
+          <Typography
+            sx={{ textDecoration: "none", fontWeight: "bold" }}
+            variant="h5"
+            color="inherit"
+            component={RouterLink}
+            to="/"
+          >
+            PSA-Codesprint-2024
+          </Typography>
+          {isAuth && (
+            <Typography variant="h6">
+              Welcome {session.getters.getUser().username}!
+            </Typography>
+          )}
+        </Stack>
+        <div>
+          {!isAuth ? (
+            <Stack spacing={2} direction="row">
+              <Button
+                variant="contained"
+                color="secondary"
+                component={RouterLink}
+                to="/login"
+              >
+                Login
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                component={RouterLink}
+                to="/register"
+              >
+                Register
+              </Button>
+            </Stack>
+          ) : (
+            <Stack spacing={2} direction="row">
+              <Button
+                variant="contained"
+                component={RouterLink}
+                to="/post_question"
+              >
+                Post Question
+              </Button>
+              <Button
+                variant="contained"
+                color="secondary"
+                onClick={() => {
+                  session.actions
+                    .logoutUser()
+                    .then(() => setIsAuth(false))
+                    .catch(() => {
+                      alert("Error logging out, refresh the page!");
+                    });
+                }}
+              >
+                Logout
+              </Button>
+            </Stack>
+          )}
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
