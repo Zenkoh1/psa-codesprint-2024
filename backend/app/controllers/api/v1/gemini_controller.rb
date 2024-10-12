8b31fc26dce6164a3282ec0d4de15563424bb03d
Zen Koh <zenkoh193@gmail.com>
2024-10-12 23:36:28 +0800

    Allow nil job-description

--- backend/app/controllers/api/v1/gemini_controller.rb
+++ backend/app/controllers/api/v1/gemini_controller.rb
@@ -4,13 +4,13 @@ class Api::V1::GeminiController < ApplicationController
     client = GeminiAi::Client.new
     workshops = Workshop.all.map { |workshop| "- Workshop ID: #{workshop.id}, Title: #{workshop.title}, Description: #{workshop.description}\n" }.join
     user_description = User.find(params[:user_id]).job_description
-    message = "My job is about" + user_description + ", recommend me one workshop from the list of workshops here\n" +
+    message = "My job is about" + (user_description || "") + ", recommend me one workshop from the list of workshops here, based on what my job is about\n" +
       workshops + "It is in the format ( - Workshop ID: <id>, Title: <title>, Description: <description>).\nOnly Return me the <id> of the workshop," +
       "do not return me the <title> or the <description> of the workshop," +
       "from the list above that you recommend me to attend, in JSON format. Don't include a \\n at the end of the JSON." + 
       "If you don't recommend any workshop, return an empty array." +
       "Recommend me a maximum of 3 workshops, ranked in order of preference."
-    
+    print(message)
     result = client.generate_content(
       {
         contents: {
