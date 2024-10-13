1347a86419e9708db28ed4e28fe077f9b16f9ee9
Zen Koh <zenkoh193@gmail.com>
2024-10-14 00:57:09 +0800

    Change gemini

--- backend/config/initializers/gemini.rb
+++ backend/config/initializers/gemini.rb
@@ -3,5 +3,5 @@ require "gemini-ai"
 GeminiAi.configure do |config|
   config.service = 'vertex-ai-api'
   config.region = 'asia-southeast1'
-  config.file_path = File.join(__dir__, '../google-credentials.json')
+  config.file_path = Rails.root.join('google-credentials.json') 
 end
